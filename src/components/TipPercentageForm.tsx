cea6c1eaf40f1e8054c6fc9d4fa18d34e6869b9a
diegotoledo13 <diegomasteruno13@gmail.com>
2024-04-16 11:44:07 -0300

    terminando el mini proyecto en TypeScript agregando alguna utilidad mas

--- src/components/TipPercentageForm.tsx
+++ src/components/TipPercentageForm.tsx
@@ -1,44 +1,58 @@
-
-type TipPercentageFromProps ={
-    setTip: React.Dispatch<React.SetStateAction<number>>
-    tip: number
-}
+type TipPercentageFromProps = {
+  setTip: React.Dispatch<React.SetStateAction<number>>;
+  tip: number;
+  removeTip: () => void;
+};
 const tipOptions = [
-    {
-      id: 'tip-10',
-      value: .10,
-      label: '10%'
-    },
-    {
-      id: 'tip-20',
-      value: .20,
-      label: '20%'
-    },
-    {
-      id: 'tip-50',
-      value: .50,
-      label: '50%'
-    },
-  ]
+  {
+    id: "tip-10",
+    value: 0.1,
+    label: "10%",
+  },
+  {
+    id: "tip-20",
+    value: 0.2,
+    label: "20%",
+  },
+  {
+    id: "tip-50",
+    value: 0.5,
+    label: "50%",
+  },
+];
 
-export default function TipPercentageForm({setTip, tip}: TipPercentageFromProps) {
+export default function TipPercentageForm({
+  setTip,
+  tip,
+  removeTip,
+}: TipPercentageFromProps) {
   return (
-    <div>
+    <>
+      <div className=" flex justify-between">
         <h3 className=" font-black text-2xl">Propina</h3>
-        <form action="">
-            {tipOptions.map(tipOptions =>(
-                <div key={tipOptions.id} className=" flex gap-2">
-                    <label htmlFor={tipOptions.id}>{tipOptions.label}</label>
-                    <input
-                     type="radio" 
-                     name="tip" 
-                     id={tipOptions.id} 
-                     value={tipOptions.value} 
-                     onChange={e => setTip(+e.target.value)} 
-                     checked={tipOptions.value === tip}/>
-                </div>
-            ))}
-        </form>
-    </div>
-  )
+        <button
+          className=" bg-red-600 h-8 w-8 rounded-full text-white disabled:opacity-10"
+          onClick={removeTip}
+          disabled={tip === 0}
+        >
+          X
+        </button>
+      </div>
+      <form action="">
+        {tipOptions.map((tipOptions) => (
+          <div key={tipOptions.id} className=" flex gap-2">
+            <label htmlFor={tipOptions.id}>{tipOptions.label}</label>
+            <input
+              type="radio"
+              name="tip"
+              id={tipOptions.id}
+              value={tipOptions.value}
+              onChange={(e) => setTip(+e.target.value)}
+              checked={tipOptions.value === tip}
+            />
+          </div>
+        ))}
+      </form>
+    </>
+  );
 }
--- src/hook/useOrder.tsx
+++ src/hook/useOrder.tsx
@@ -1,35 +1,42 @@
-import {useState} from 'react'
-import type { MenuItem, OrderItem } from '../types'
+import { useState } from "react";
+import type { MenuItem, OrderItem } from "../types";
 
 export default function useOrder() {
-    const [order, setOrder] = useState<OrderItem[]>([])
-    const [tip, setTip] = useState(0)
+  const [order, setOrder] = useState<OrderItem[]>([]);
+  const [tip, setTip] = useState(0);
 
-
-    const addItem = (item:MenuItem) => {
-        const itemExist = order.find((orderItem) => orderItem.id === item.id)
-        if(itemExist){
-            const upDateOrder = order.map((orderItem) => (orderItem.id === item.id ? {...orderItem, quantity: orderItem.quantity + 1} : orderItem))
-            setOrder(upDateOrder)
-        } else{            
-            const newItem = {...item, quantity: 1}
-            setOrder([...order, newItem])
-        }
-     }
-     const removeItem = (id: MenuItem['id']) =>{
-        setOrder(order.filter((orderItem) => orderItem.id !== id))
-     }
-     const placeOrder = () => {
-        alert('Orden guardada')
-        setOrder([])
-        setTip(0)
-     }
-    return {
-        addItem,
-        tip,
-        setTip,
-        order,
-        removeItem,
-        placeOrder
+  const addItem = (item: MenuItem) => {
+    const itemExist = order.find((orderItem) => orderItem.id === item.id);
+    if (itemExist) {
+      const upDateOrder = order.map((orderItem) =>
+        orderItem.id === item.id
+          ? { ...orderItem, quantity: orderItem.quantity + 1 }
+          : orderItem
+      );
+      setOrder(upDateOrder);
+    } else {
+      const newItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
     }
+  };
+  const removeItem = (id: MenuItem["id"]) => {
+    setOrder(order.filter((orderItem) => orderItem.id !== id));
+  };
+  const placeOrder = () => {
+    alert("Orden guardada");
+    setOrder([]);
+    setTip(0);
+  };
+  const removeTip = () => {
+    setTip(0);
+  };
+  return {
+    addItem,
+    tip,
+    setTip,
+    order,
+    removeItem,
+    placeOrder,
+    removeTip,
+  };
 }
